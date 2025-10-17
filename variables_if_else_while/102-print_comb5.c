197eb12f42fff9999d73488223296614a260aa60
LuisJimenez <luisssajs@gmail.com>
2025-10-17 19:23:56 +0000

    combination of 4

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -1,33 +1,29 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * Prints all possible different combinations of two two-digit numbers
+ * Prints all possible combinations of two two-digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int a, b, c, d;
-for (a = 0; a <= 9; a++)
+int a, b;
+for (a = 0; a <= 98; a++)
 {
-for (b = 0; b <= 9; b++)
+for (b = a + 1; b <= 99; b++)
 {
-for (c = 0; c <= 9; c++)
-{
-for (d = 1; d <= 9; d++)
-{
-putchar(a + '0');
-putchar(b + '0');
+putchar(a / 10 + '0');
+putchar(a % 10 + '0');
 putchar(' ');
-putchar(c + '0');
-putchar(d + '0');
-if (a != 9 || b != 9 || c != 9 || d != 9)
+putchar(b / 10 + '0');
+putchar(b % 10 + '0');
+if (a != 98 || b != 99)
 {
 putchar(',');
 putchar(' ');
 }
 }
 }
-}
-}
+putchar('\n');
 return (0);
 }
