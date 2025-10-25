9110e97373887bc61c247ee76cf8b3320b079259
LuisJimenez <luisssajs@gmail.com>
2025-10-25 01:31:31 +0000

    Puts x2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * puts2 - prints every other character of a string
@@ -7,13 +6,12 @@
  */
 void puts2(char *str)
 {
-    int i = 0;
+int i;
 
-    while (str[i] != '\0')
-    {
-        if (i % 2 == 0)
-            putchar(str[i]);
-        i++;
-    }
-    putchar('\n');
+for (i = 0; str[i] != '\0'; i++)
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+}
+_putchar('\n');
 }
