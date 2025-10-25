30bad629fb9cf0b23215956c43964c726db58541
LuisJimenez <luisssajs@gmail.com>
2025-10-25 02:26:01 +0000

    Array to int

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 
 /**
  * _atoi - converts a string to an integer
@@ -9,21 +10,35 @@ int _atoi(char *s)
 {
 int i = 0;
 int sign = 1;
-int result = 0;
+unsigned int result = 0;
 int found = 0;
 
 while (s[i] != '\0')
 {
 if (s[i] == '-')
+{
 sign *= -1;
+}
 else if (s[i] >= '0' && s[i] <= '9')
 {
+if (result > (INT_MAX - (s[i] - '0')) / 10)
+{
+if (sign == 1)
+return (INT_MAX);
+else
+return (INT_MIN);
+}
 result = result * 10 + (s[i] - '0');
 found = 1;
 }
 else if (found)
+{
 break;
+}
 i++;
 }
-return (result *sign);
+
+if (sign == -1)
+return (-((int)result));
+return ((int)result);
 }
