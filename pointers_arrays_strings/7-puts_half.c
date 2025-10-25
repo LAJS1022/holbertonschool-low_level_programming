45214511302b78d516ba487b10da028b338fbcd2
LuisJimenez <luisssajs@gmail.com>
2025-10-25 01:35:17 +0000

    Puts half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: input string
+ */
+void puts_half(char *str)
+{
+int i = 0;
+int start;
+
+while (str[i] != '\0')
+i++;
+
+if (i % 2 == 0)
+start = i / 2;
+else
+start = (i + 1) / 2;
+
+while (str[start] != '\0')
+{
+_putchar(str[start]);
+start++;
+}
+_putchar('\n');
+}
