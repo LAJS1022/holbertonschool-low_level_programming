121453dafcb84c750bb276d5784ff575dfe1e9b6
LuisJimenez <luisssajs@gmail.com>
2025-11-13 17:24:40 +0000

    print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
  * print_all - prints anything based on format
@@ -12,36 +13,30 @@ void print_all(const char * const format, ...)
 va_list args;
 unsigned int i = 0;
 char *s;
-char sep = 0;
+char *sep = "";
 
 va_start(args, format);
 while (format && format[i])
 {
-if (sep)
-printf(", ");
 switch (format[i])
 {
 case 'c':
-printf("%c", va_arg(args, int));
-sep = 1;
+printf("%s%c", sep, va_arg(args, int));
 break;
 case 'i':
-printf("%d", va_arg(args, int));
-sep = 1;
+printf("%s%d", sep, va_arg(args, int));
 break;
 case 'f':
-printf("%f", va_arg(args, double));
-sep = 1;
+printf("%s%f", sep, va_arg(args, double));
 break;
 case 's':
 s = va_arg(args, char *);
-if (s == 0)
-printf("(nil)");
-else
-printf("%s", s);
-sep = 1;
+if (s == NULL)
+s = "(nil)";
+printf("%s%s", sep, s);
 break;
 }
+sep = ", ";
 i++;
 }
 va_end(args);
