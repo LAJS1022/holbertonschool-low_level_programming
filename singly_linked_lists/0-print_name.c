f35c523d9d29f738f50849260c4286a608a8ddf9
LuisJimenez <luisssajs@gmail.com>
2025-11-25 16:46:11 +0000

    task 1

---
+++ singly_linked_lists/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name using a function pointer
+ * @name: name to print
+ * @f: function pointer to printing function
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name != 0 && f != 0)
+f(name);
+}
