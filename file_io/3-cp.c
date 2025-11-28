2c80984af1835b876594d4cb7a06957b2b90ede1
LuisJimenez <luisssajs@gmail.com>
2025-11-28 17:49:11 +0000

    task 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,7 +3,6 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
  * error_exit - prints an error message and exits with given code
  * @code: exit code
@@ -15,7 +14,6 @@ void error_exit(int code, const char *msg, const char *arg)
 dprintf(STDERR_FILENO, msg, arg);
 exit(code);
 }
-
 /**
  * main - copies the content of a file to another file
  * @argc: number of arguments
@@ -28,42 +26,34 @@ int main(int argc, char *argv[])
 int fd_from, fd_to;
 ssize_t r, w;
 char buf[1024];
-
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-
 fd_from = open(argv[1], O_RDONLY);
 if (fd_from == -1)
 error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-
 fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (fd_to == -1)
 error_exit(99, "Error: Can't write to %s\n", argv[2]);
-
 while ((r = read(fd_from, buf, 1024)) > 0)
 {
 w = write(fd_to, buf, r);
 if (w == -1 || w != r)
 error_exit(99, "Error: Can't write to %s\n", argv[2]);
 }
-
 if (r == -1)
 error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-
 if (close(fd_from) == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 exit(100);
 }
-
 if (close(fd_to) == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 exit(100);
 }
-
 return (0);
 }
